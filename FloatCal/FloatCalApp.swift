51a602040c5078d0789d94b144961e59ab72a6d5
mdzsmi950-ux <mdzsmi950@gmail.com>
2026-05-22 23:44:09 -0500

    Initial Commit

---
+++ FloatCal/FloatCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatCalApp.swift
+//  FloatCal
+//
+//  Created by Maddie Smith on 5/22/26.
+//
+
+import SwiftUI
+
+@main
+struct FloatCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FloatCalTests/FloatCalTests.swift
@@ -0,0 +1,19 @@
+//
+//  FloatCalTests.swift
+//  FloatCalTests
+//
+//  Created by Maddie Smith on 5/22/26.
+//
+
+import Testing
+@testable import FloatCal
+
+struct FloatCalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
